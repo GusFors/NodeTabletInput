1a52a596dec50639f089fd56a8e89bc3949d45df
Gustav Forselius <gf222cy@student.lnu.se>
2021-04-23 03:22:22 +0200

    adding rough data conversion and adding pen input

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let HID = require('node-hid')
+let robot = require('robotjs')
 
 if (process.argv[2] === 'd') {
   let devices = HID.devices()
@@ -7,10 +8,20 @@ if (process.argv[2] === 'd') {
 }
 
 let tabletDevice = new HID.HID('\\\\?\\hid#vid_056a&pid_030e&col02#7&26e2e4fd&1&0001#{4d1e55b2-f16f-11cf-88cb-001111000030}')
+robot.setMouseDelay(0)
 
 let intervalData
-tabletDevice.on('data', (data) => {
-  intervalData = data
+let x
+let y
+
+tabletDevice.on('data', (reportData) => {
+  intervalData = reportData
+
+  x = reportData[3] | (reportData[4] << 8)
+  y = reportData[5] | (reportData[6] << 8)
+
+  robot.moveMouse(Math.round(x * 0.16842), Math.round(y * 0.1515789))
+  //console.log(`xScreen: ${Math.round(x * 0.16842)} yScreen: ${Math.round(y * 0.1515789)}`)
 })
 
 setInterval(() => {
@@ -20,10 +31,16 @@ setInterval(() => {
   console.log(`raw yPosition:`, intervalData.slice(5, 7))
   console.log(`reportID: ${intervalData[2] >> 1}`) // reportID
 
-  let x = intervalData[3] | (intervalData[4] << 8)
-  let y = intervalData[5] | (intervalData[6] << 8)
+  // x = intervalData[3] | (intervalData[4] << 8)
+  // y = intervalData[5] | (intervalData[6] << 8)
   //intervalData.slice(3, 5).reverse() gammalt sätt
   //intervalData.slice(5, 7).reverse()
 
-  console.log(`current Position: [x:`, x, ', y:', y, ']')
+  console.log(`current unscaled Position: [x:`, x, ', y:', y, ']')
+  console.log(`xScreen: ${Math.round(x * 0.16842)} yScreen: ${Math.round(y * 0.1515789)}`)
 }, 100)
+
+setTimeout(() => {
+  process.exit()
+  //robot.moveMouse(0, 0)
+}, 6000)
