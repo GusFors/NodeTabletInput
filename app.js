c3002effe6b5d6c8d45c595affdedd8103f1eb59
Gustav Forselius <gf222cy@student.lnu.se>
2021-04-23 14:17:52 +0200

    adding pen tip logging

--- app.js
+++ app.js
@@ -25,13 +25,24 @@ tabletDevice.on('data', (reportData) => {
   //console.log(`xScreen: ${Math.round(x * 0.16842)} yScreen: ${Math.round(y * 0.1515789)}`)
 })
 
+tabletDevice.on('data', (reportData) => {
+  intervalData = reportData
+
+  x = reportData[3] | (reportData[4] << 8)
+  y = reportData[5] | (reportData[6] << 8)
+
+  x === 0 ? false : y === 0 ? false : robot.moveMouse(Math.round(x * 0.16842), Math.round(y * 0.1515789))
+
+  //console.log(`xScreen: ${Math.round(x * 0.16842)} yScreen: ${Math.round(y * 0.1515789)}`)
+})
+
 setInterval(() => {
   console.clear()
   console.log(`Full Raw data:`, intervalData)
   console.log(`raw xPosition: `, intervalData.slice(3, 5))
   console.log(`raw yPosition:`, intervalData.slice(5, 7))
   console.log(`reportID: ${intervalData[2] >> 1}`) // reportID
-
+  console.log(`Pen tip is pressed: ${intervalData[2] === 241}`)
   // x = intervalData[3] | (intervalData[4] << 8)
   // y = intervalData[5] | (intervalData[6] << 8)
   //intervalData.slice(3, 5).reverse() gammalt sätt
