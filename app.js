38743bda09f4d584167d2d1f9ea1a4a7e1d4c5e5
Gustav Forselius <gf222cy@student.lnu.se>
2021-04-25 18:57:23 +0200

    click "hack"

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ draftLog(console)
 
 let isForcedProportions = process.argv.includes('-f')
 let isFastLogging = process.argv.includes('-l')
+let isDraftLog = process.argv.includes('-d')
+let isExit = process.argv.includes('-t')
 
 let yScale
-let xScale = 0.16842105263157894736842105263158
-isForcedProportions ? (yScale = 0.16842105263157894736842105263158) : (yScale = 0.15157894736842105263157894736842)
+let xScale = 2560 / 15200 //  0.16842105263157894736842105263158
+isForcedProportions ? (yScale = 1440 / 8550) : (yScale = 1440 / 9500)
 
 let config = {
   vendorId: 1386,
@@ -24,87 +26,102 @@ let config = {
   usage: 10,
 }
 
-if (process.argv.includes('-d')) {
+robot.setMouseDelay(0)
+
+if (process.argv.includes('-de')) {
   let devices = HID.devices()
   console.log(devices)
   //tabletDevice.getFeatureReport(64, 11)
   process.exit()
 }
 
-let tabletDevice = new HID.HID(config.path)
-robot.setMouseDelay(0)
+const tabletDevice = new HID.HID(config.path)
 
 let intervalData
 let x
 let y
 let reportsPerSec = 0
+let isClickHold = false
 
-setInterval(() => {
-  repPerSecUpdate('RPS: ~' + reportsPerSec)
-  reportsPerSec = 0
-}, 1000)
-
-// setTimeout(() => {
-//   robot.moveMouse(0,0)
-// }, 2000);
+if (isDraftLog) {
+  let repPerSecUpdate = console.draft('RPS:')
+  setInterval(() => {
+    repPerSecUpdate('RPS: ~' + reportsPerSec)
+    reportsPerSec = 0
+  }, 1000)
+}
 
 tabletDevice.on('data', (reportData) => {
   intervalData = reportData
   reportsPerSec++
 
-  x = reportData[3] | (reportData[4] << 8) | ((reportData[9]) & 1)
+  x = reportData[3] | (reportData[4] << 8)
   y = reportData[5] | (reportData[6] << 8)
 
   if (y > 8550 && isForcedProportions) {
     y = 8550
   }
-  //console.log(x)
-  // TODO x=0 kommer för tidigt
-  x === 0 ? false : y === 0 ? false : robot.moveMouse(x * xScale, y * yScale) // fixa så att ett 0-värde inte blockerar det andra värdet från att sättas
-  //  intervalData[2] === 241 ? robot.mouseClick('left', false) : false // basically autoclicker atm, TODO fix hold instead
 
-  if (reportData[2] === 241) {
-    robot.mouseToggle('down', 'left')
+  xS = x * xScale
+  yS = y * yScale
+
+  //  intervalData[2] === 241 ? robot.mouseClick('left', false) : false // basically autoclicker atm, TODO fix hold instead
+  x === 0 && y === 0 ? false : robot.moveMouse(xS, yS) // fixa så att ett 0-värde inte blockerar det andra värdet från att sättas
+
+  // TODO fix hold instead of this crap
+  if (reportData[2] > 241) {
+    if (isClickHold === false) {
+      robot.mouseClick('right', false)
+      setTimeout(() => {
+        isClickHold = false
+      }, 500)
+    }
+    isClickHold = true
   }
 
-  // console.log(reportData[2] !== 241)
-  if (reportData[2] === 240) {
-    robot.mouseToggle('up', 'left')
+  if (reportData[2] === 241) {
+    if (isClickHold === false) {
+      robot.mouseClick('left', false)
+      setTimeout(() => {
+        isClickHold = false
+      }, 500)
+    }
+    isClickHold = true
   }
 })
 
-let rawUpdate = console.draft('')
-let xPosUpdate = console.draft('')
-let yPosUpdate = console.draft('')
-let unscaledUpdate = console.draft('')
-let screenPosUpdate = console.draft('')
-let reportIdUpdate = console.draft('')
-let penTipUpdate = console.draft('')
-let forcedPropUpdate = console.draft(`forcedProportions:`, isForcedProportions ? `${isForcedProportions}`.green : `${isForcedProportions}`.red)
-let repPerSecUpdate = console.draft('RPS:')
-let outsideAreaUpdate = console.draft('outside:')
+if (isDraftLog) {
+  let rawUpdate = console.draft('')
+  let xPosUpdate = console.draft('')
+  let yPosUpdate = console.draft('')
+  let unscaledUpdate = console.draft('')
+  let screenPosUpdate = console.draft('')
+  let reportIdUpdate = console.draft('')
+  let penTipUpdate = console.draft('')
+  let forcedPropUpdate = console.draft(`forcedProportions:`, isForcedProportions ? `${isForcedProportions}`.green : `${isForcedProportions}`.red)
+  let outsideAreaUpdate = console.draft('outside:')
+  let currentColor = console.draft('color:')
+  setInterval(
+    () => {
+      rawUpdate(`Full Raw data:`, intervalData)
+      xPosUpdate(`raw xPosition: `, intervalData.slice(3, 5))
+      yPosUpdate(`raw yPosition:`, intervalData.slice(5, 7))
 
-setInterval(
-  () => {
-    rawUpdate(`Full Raw data:`, intervalData)
-    xPosUpdate(`raw xPosition: `, intervalData.slice(3, 5))
-    yPosUpdate(`raw yPosition:`, intervalData.slice(5, 7))
+      unscaledUpdate(`current unscaled Position: [x:`, x, ', y:', y, ']')
+      screenPosUpdate(`xScreen: ${Math.round(x * xScale)} yScreen: ${Math.round(y * yScale)}`)
+      reportIdUpdate(`reportID: ${intervalData[2] >> 1}`) // reportID
 
-    unscaledUpdate(`current unscaled Position: [x:`, x, ', y:', y, ']')
-    screenPosUpdate(`xScreen: ${Math.round(x * xScale)} yScreen: ${Math.round(y * yScale)}`)
-    reportIdUpdate(`reportID: ${intervalData[2] >> 1}`) // reportID
-
-    penTipUpdate(`Pen tip is pressed:`, intervalData[2] === 241 ? `${intervalData[2] === 241}`.green : `${intervalData[2] === 241}`.red)
-    outsideAreaUpdate(intervalData.slice(9, 10))
-    //console.log(`forcedProportions:`, isForcedProportions ? `${isForcedProportions}`.green : `${isForcedProportions}`.red)
-    //intervalData.slice(3, 5).reverse() gammalt sätt
-    //intervalData.slice(5, 7).reverse()
-  },
-  isFastLogging ? 50 : 100
-)
+      penTipUpdate(`Pen tip is pressed:`, intervalData[2] === 241 ? `${intervalData[2] === 241}`.green : `${intervalData[2] === 241}`.red)
+      outsideAreaUpdate(intervalData.slice(9, 10))
+    },
+    isFastLogging ? 50 : 100
+  )
+}
 
 // in case of being unable to exit while testing
-setTimeout(() => {
-  process.exit()
-  //robot.moveMouse(0, 0)
-}, 300000)
+if (isExit) {
+  setTimeout(() => {
+    process.exit()
+    //robot.moveMouse(0, 0)
+  }, 900000)
+}
