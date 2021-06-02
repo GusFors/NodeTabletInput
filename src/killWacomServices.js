85f6d1bbe60a035c9f5e712b85eb73709a9d12af
Gustav Forselius <gf222cy@student.lnu.se>
2021-06-03 00:35:33 +0200

    adding killWacomServices

---
+++ src/killWacomServices.js
@@ -0,0 +1,26 @@
+const { exec } = require('child_process')
+
+// needs to be run as admin
+exec('net start WTabletServicePro', (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`)
+    return
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`)
+    return
+  }
+  console.log(`stdout: ${stdout}`)
+})
+
+exec('taskkill /F /IM Wacom_Tablet.exe', (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`)
+    return
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`)
+    return
+  }
+  console.log(`stdout: ${stdout}`)
+})
